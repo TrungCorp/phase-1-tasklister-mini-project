eb3ba28b4738a438961387d42f17f66afc2a947d
TrungLe <trungle9995@gmail.com>
2023-11-20 11:10:04 -0500

    complete labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const list = document.getElementById('tasks')
 document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
   // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let ele = e.target['new-task-description'].value
+    console.log(ele)
+    submitText(ele)
+    
+    
+  })
+
 });
+function submitText(text){
+  let p = document.createElement('p')
+  p.textContent = text
+  document.getElementById('tasks').appendChild(p)
+}
